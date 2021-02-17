2825df1569d9d705bd061e704b07c81afcb039a7
VieleSchritte <ohheavenineed@gmail.com>
2021-02-17 16:11:41 +0300

    created first three needed models

--- bioblog_tasks/admin.py
+++ bioblog_tasks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Comment, BlogPost
 
+admin.site.register(User)
+admin.site.register(Comment)
+admin.site.register(BlogPost)
 # Register your models here.
--- bioblog_tasks/models.py
+++ bioblog_tasks/models.py
@@ -1,3 +1,40 @@
+from __future__ import unicode_literals
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    e_mail = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
+    published_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.content
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=2000)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.content
